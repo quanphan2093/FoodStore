a5f4f5f0bf13a61eb02d886544a3861c699d4eef
Ducnm21 <ducnguyenm.to@gmail.com>
2025-04-21 20:47:39 +0700

    Update Controller + DAO

---
+++ FoodStoreAPI/Controllers/Admin/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FoodStoreAPI.DAO;
+using FoodStoreAPI.DTO;
+
+namespace FoodStoreAPI.Controllers.Admin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : Controller
+    {
+        [HttpGet("Admin/Accounts")]
+        public ActionResult<IEnumerable<AccountLDTO>> GetAccount()
+        {
+            return AccountDAO.getAccount();
+        }
+
+        [HttpGet("Admin/Account/Search")]
+        public ActionResult<IEnumerable<AccountLDTO>> GetAccountSearch(string searchAccount)
+        {
+            return AccountDAO.getAccountSearch(searchAccount);
+        }
+
+        [HttpPut("Admin/Update/Account")]
+        public IActionResult UpdateRoleAccount(int accId, int roleId)
+        {
+            AccountDAO.UpdateRoleAccount(accId, roleId);
+            return Ok();
+        }
+
+    }
+}
