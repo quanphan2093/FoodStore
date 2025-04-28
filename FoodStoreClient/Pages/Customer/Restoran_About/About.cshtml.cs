6380786ac70d52203706cfb39b3f93dbd8f8e8b4
quanphan2k3 <quanpahe170260@fpt.edu.vn>
2025-04-28 21:35:25 +0700

    fix active link

--- FoodStoreClient/Pages/Customer/Restoran_About/About.cshtml.cs
+++ FoodStoreClient/Pages/Customer/Restoran_About/About.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace FoodStoreClient.Pages.Guest.Restoran_About
+namespace FoodStoreClient.Pages.Customer.Restoran_About
 {
     public class AboutModel : PageModel
     {
--- FoodStoreClient/Pages/Customer/Restoran_Team/Team.cshtml.cs
+++ FoodStoreClient/Pages/Customer/Restoran_Team/Team.cshtml.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace FoodStoreClient.Pages.Guest.Restoran_About
+namespace FoodStoreClient.Pages.Customer.Restoran_Team
 {
-    public class AboutModel : PageModel
+    public class TeamModel : PageModel
     {
         public void OnGet()
         {
